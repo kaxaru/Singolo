78b388f70719bd5fe8fb3037fcb19588d1ee8774
Kaxaru <khidechi@gmail.com>
2020-03-14 13:15:21 +0300

    feat: add slider

--- dev/main.js
+++ dev/main.js
@@ -7,7 +7,55 @@
         }
     })
 
-    
+    // 2 Slider
+
+    let slider = (setting) => {
+        let configure = {};
+        configure.setting = setting;
+        configure.dom = {
+            "main": document.querySelector(configure.setting.main),
+            "wrap": document.querySelector(configure.setting.wrap),
+            "children": document.querySelector(configure.setting.wrap).children,
+            "prev": document.querySelector(configure.setting.prev),
+            "next": document.querySelector(configure.setting.next)
+        }
+
+        configure.addSetting = {
+            "position": 0,
+            "max_position": document.querySelector(configure.setting.wrap).children.length
+        }
+
+        configure.nextSlide = () => {
+            ++configure.addSetting.position;
+            if(configure.addSetting.position >= configure.addSetting.max_position) {
+                    configure.addSetting.position = 0
+            }
+
+            configure.dom.wrap.style["transform"] = `translateX(-${configure.addSetting.position}00%)`
+        }
+
+        configure.prevSlide = () => {
+            --configure.addSetting.position;
+            if(configure.addSetting.position <= 0) {
+                configure.addSetting.position = 0
+            }
+
+            configure.dom.wrap.style["transform"] = `translateX(-${configure.addSetting.position}00%)`
+        }
+
+
+        if(configure.dom.prev !== null) {
+            configure.dom.prev.addEventListener('click', () => {
+                configure.prevSlide();
+            })
+        }
+
+        if(configure.dom.next !== null) {
+            configure.dom.next.addEventListener('click', () => {
+                configure.nextSlide();
+            })
+        }
+    }
 
     // 3 Slider: activate phones
     document.querySelector('.phone-slider').addEventListener("click", function(e){
@@ -16,6 +64,13 @@
             elem.parentElement.querySelector('.invisible').classList.remove('invisible')
             elem.classList.add('invisible')
         }
+    })
+
+    slider({
+        "main": "section.phone",
+        "wrap": ".phone-slider__wrapper",
+        "prev": ".prev",
+        "next": ".next"
     })
 
     // 4 switch tabs
